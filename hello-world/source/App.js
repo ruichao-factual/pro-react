6cfaa807820f15c5992ab341638aa50a9826508b
ruichao-factual <ruichao@factual.com>
2016-03-10 11:22:06 +0800

    chapter1 simple project

---
+++ hello-world/source/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+class Hello extends React.Component {
+  render() {
+    return (
+      <h1>Hello World</h1>
+    );
+  }
+}
+
+ReactDom.render(<Hello />, document.getElementById('root'));
