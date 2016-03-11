40d87cc49751b42c83247882c7c60eccdc704ca7
ruichao-factual <ruichao@factual.com>
2016-03-11 11:05:58 +0800

    move event handle function outof jsx

--- kanban/app/Card.js
+++ kanban/app/Card.js
@@ -16,6 +16,10 @@ class Card extends Component {
     };
   }
 
+  toggleDetails() {
+    this.setState({showDetails: !this.state.showDetails});
+  }
+
 
   render() {
     let cardDetails;
@@ -30,11 +34,9 @@ class Card extends Component {
 
     return (
       <div className='card'>
-        <div className='card_title'
-          onClick={
-            () => this.setState({showDetails: !this.state.showDetails})
-          }
-        >{this.props.title}</div>
+        <div className='card_title' onClick={this.toggleDetails.bind(this)}>
+          {this.props.title}
+        </div>
         {cardDetails}
       </div>
     );
